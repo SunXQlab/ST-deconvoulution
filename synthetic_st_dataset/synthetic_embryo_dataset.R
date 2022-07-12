ac85f97b90ed299df0c392b613565ace0300987a
luluyan <luluyan0329@163.com>
2022-07-12 16:16:11 +0800

    syn commit

---
+++ synthetic_st_dataset/synthetic_embryo_dataset.R
@@ -0,0 +1,159 @@
+setwd("/home/yll/benchmark_github/Dataset/embryo_sci-Space_raw")
+library(readr)
+library(Matrix)
+library(dplyr)
+library(vec2dtransf)
+library(tidyr)
+library(tibble)
+
+#load raw data
+cell_meta <- read_tsv('GSE166692_sciSpace_cell_metadata_new.tsv')
+gene_meta <- read_tsv('GSE166692_sciSpace_gene_metadata.tsv')
+count_mat <- Matrix::readMM('GSE166692_sciSpace_count_matrix.mtx')
+table(cell_meta$slide_id)
+
+#remove cell type with cell number < 25 
+pos_OPCs <- which(cell_meta$final_cluster_label == "OPCs")
+pos_Testis <- which(cell_meta$final_cluster_label == "Testis Cells")
+pos <- c(pos_OPCs,pos_Testis)
+cell_meta <- cell_meta[-pos,]
+count_mat <- count_mat[,-pos]
+colnames(count_mat) <- cell_meta$Cell
+
+#selected silde14 data
+slide14_cell_meta <- subset.data.frame(cell_meta,
+                                       cell_meta$slide_id=="Slide 14")
+rownames(slide14_cell_meta) <- NULL
+pos_slide14 <- which(cell_meta$slide_id=="Slide 14")#根据slide-14在cell_metadata中的位置选出slide_14对应的counts和gene_name
+slide14_count_mat <- count_mat[,pos_slide14]
+
+slide14_cell_meta <- subset.data.frame(cell_meta,cell_meta$slide_id=="Slide 14")
+rownames(slide14_cell_meta) <- NULL
+pos_slide14 <- which(cell_meta$slide_id=="Slide 14")#根据slide-14在cell_metadata中的位置选出slide_14对应的counts和gene_name
+slide14_count_mat <- count_mat[,pos_slide14]
+
+#obtain gene name
+n <- length(gene_meta$gene_short_name)
+gene <- lapply(1:n,function(x){
+  strsplit(gene_meta$gene_short_name[x],"\\t")[[1]][2]
+})
+gene <- unlist(gene) #对list进行unlist会自动把里面的字符变为一个list中的向量
+rownames(slide14_count_mat) <- gene
+colnames(slide14_count_mat) <- slide14_cell_meta$Cell
+
+#obtain spatial coordinate
+x_slide14 <- slide14_cell_meta$Row 
+y_slide14 <- slide14_cell_meta$Col 
+x_slide14 <- as.data.frame(x_slide14)
+y_slide14 <- as.data.frame(y_slide14)
+cell_coords <- mutate(x_slide14,y_slide14) 
+colnames(cell_coords) <- c("Row","Col")
+
+unique_coords <- unique(paste(cell_coords$Row,cell_coords$Col,sep=","))
+celltype_list <- list()  #用来存储celltype的构成信息
+local_info_list <- list() #用来存储每个位置对应的cellname信息
+for (k in unique_coords){
+  
+  pos_cell = which(cell_coords$Row==strsplit(k,split=",")[[1]][1] & cell_coords$Col==strsplit(k,split=",")[[1]][2])
+  local_celltype = cell_meta$final_cluster_label[pos_cell]
+  local_info = as.data.frame(cbind(pos_cell,local_celltype))
+  local_info_list[[k]] = local_info  #包括pos和pos对应的celltype信息 
+  
+  celltype_number = as.data.frame(table(cell_meta$final_cluster_label[pos_cell]))
+  cell_prop = celltype_number$Freq/sum(celltype_number$Freq)
+  celltype_info = as.data.frame(cbind(celltype_number,cell_prop))
+  celltype_list[[k]] = celltype_info
+}
+
+#generate simulated spot by synthesising cells with the same coordinates
+ds_spot<-lapply(unique_coords,function(n){
+  
+  #一个位置上只有一个cell的情况
+  if (dim(celltype_list[[n]])[1]==1){
+    
+    #local_info_list[[n]][1]是data.frame,里面的pos_cell是字符型，不能用于提取矩阵的某一列，需要变为数字型
+    pos_spot<-as.numeric(unlist(local_info_list[[n]][1]))
+    prop<-celltype_list[[n]][3]
+    syn_spot_count<-slide14_count_mat[,pos_spot]
+    name_simp <- paste("spot_", n, sep = "")
+    
+    tmp_ds<-celltype_list[[n]] 
+    colnames(tmp_ds)<-c("cluster","Freq","cell_prop")
+    spot_ds <- tmp_ds %>%
+      dplyr::select(cluster,cell_prop) %>%
+      tidyr::pivot_wider(names_from = cluster,
+                         values_from = cell_prop) %>%
+      dplyr::mutate(name = name_simp)
+    
+    syn_spot <- rowSums(as.matrix(syn_spot_count)); sum(syn_spot)
+    
+  }else{
+    
+    # 一个位置上有多个cells的情况
+    #celltype_select<-celltype_list[[n]][which(celltype_list[[n]]$Freq>=2),]
+    #celltype_name_spot<-as.character(celltype_select$Var1) # 字符串
+    
+    # %in%：用于判断前一个向量的元素是否在后一个向量中,class(pos_select)=data.frame
+    #pos_select<-local_info_list[[n]][which(local_info_list[[n]]$local_celltype %in% celltype_name_spot),]
+    
+    #随机选取2-10个cells,class(pos_select$pos_cell)=character
+    pos_spot<-as.numeric(unlist(local_info_list[[n]][1]))
+    prop<-celltype_list[[n]][3]
+    #tmp_ds<-cell_metadata_new[pos_spot,] %>% mutate(prop)
+    syn_spot_count<-slide14_count_mat[,pos_spot]
+    name_simp <- paste("spot_", n, sep = "")
+    
+    tmp_ds<-celltype_list[[n]] 
+    colnames(tmp_ds)<-c("cluster","Freq","cell_prop")
+    spot_ds <- tmp_ds %>%
+      dplyr::select(cluster,cell_prop) %>%
+      tidyr::pivot_wider(names_from = cluster,
+                         values_from = cell_prop) %>%
+      dplyr::mutate(name = name_simp)
+    
+    syn_spot <- rowSums(as.matrix(syn_spot_count)); sum(syn_spot)
+    
+  }
+  
+  return(list(syn_spot, spot_ds))
+})
+
+#generate simulated ST metadata
+ds_spot_metadata <- purrr::map(ds_spot, 2) %>% #选取ds_spot的第二列
+  dplyr::bind_rows() %>%
+  data.frame()
+ds_spot_metadata[is.na(ds_spot_metadata)] <- 0
+
+ds_spot_metadata <- select(ds_spot_metadata,name, Connective.Tissue.Progenitors, everything())
+
+ds_spot_count <- purrr::map(ds_spot,1) %>%
+  dplyr::bind_cols() %>%
+  data.frame()
+
+#generate spot name
+ds_spot_name<-list()
+for (k in unique_coords){
+  name = paste("spot_", k, sep = "")
+  ds_spot_name[[k]] = name
+}
+ds_spot_name <- unlist(ds_spot_name)
+colnames(ds_spot_count) <- ds_spot_name
+
+#remove repeated gene
+ds_count <- mutate(ds_spot_count,GENE=gene)
+ds_count_unique <- aggregate(ds_count,by=list(ds_count$GENE),FUN=mean,na.rm=TRUE)
+ds_spot_unique <- ds_count_unique[,-1395]#去掉最后一列gene名
+ds_spot_unique <- column_to_rownames(ds_count_unique,var="Group.1")#去掉第一列group.1
+ds_spot_unique <- ds_spot_unique[,-1394]
+
+#spatial count data and meta data
+slide14_spatial_count <- ds_spot_unique
+
+#single cell count data and metadata
+
+
+
+
+
+
+
---
+++ synthetic_st_dataset/synthetic_mousebrain_Mapping_dataset.R
@@ -0,0 +1,151 @@
+setwd("/home/yll/scRNA-seq_Mapping_to_STdata")
+# library(devtools)
+# install_github("navinlabcode/CellTrek")
+
+# if (!require("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("ConsensusClusterPlus")
+
+library(CellTrek)
+library(dplyr)
+library(Seurat)
+library(viridis)
+library(ConsensusClusterPlus)
+library(tidyr)
+
+output_path = getwd()
+## using cellTek mapping single cells to spatial location
+#load raw data
+brain_st_cortex <- readRDS("brain_st_cortex.rds")#31053gene, 1075 spots
+brain_sc <- readRDS("brain_sc.rds")#34617genes, 4785 cells
+
+## Rename the cells/spots with syntactically valid names
+brain_st_cortex <- RenameCells(brain_st_cortex, new.names=make.names(Cells(brain_st_cortex)))
+brain_sc <- RenameCells(brain_sc, new.names=make.names(Cells(brain_sc)))
+
+## co-embed ST and scRNA-seq datasets using traint
+brain_traint <- CellTrek::traint(st_data=brain_st_cortex, 
+                                 sc_data=brain_sc, 
+                                 sc_assay='RNA', 
+                                 cell_names='cell_type')
+
+brain_celltrek <- CellTrek::celltrek(st_sc_int=brain_traint, int_assay='traint', 
+                                     sc_data=brain_sc, sc_assay = 'RNA', 
+                                     reduction='pca', intp=T, intp_pnt=5000, 
+                                     intp_lin=F, nPCs=30, ntree=1000, 
+                                     dist_thresh=0.55, top_spot=5, 
+                                     spot_n=5, repel_r=20, repel_iter=20, keep_model=T)$celltrek
+
+brain_celltrek$cell_type <- factor(brain_celltrek$cell_type, levels=sort(unique(brain_celltrek$cell_type)))
+id_raw <- brain_celltrek_test@meta.data$id_raw
+counts <- brain_sc@assays$RNA@counts[,id_raw]
+brain_celltrek@assays$RNA@counts <- counts
+
+## generate synthetic ST data 
+#set spot size
+patch_size <- 105
+sc_coord <- brain_celltrek@images$anterior1@coordinates
+
+x_edges <- seq(min(sc_coord$imagerow), max(sc_coord$imagerow), patch_size)
+inner_x_edges <- x_edges[2:(length(x_edges) - 1)]
+y_edges <- seq(min(sc_coord$imagecol), max(sc_coord$imagecol), patch_size)
+inner_y_edges <- y_edges[2:(length(y_edges) - 1)]
+
+sc_coord$patch_id <- character(length(rownames(sc_coord)))
+for (x in inner_x_edges) {
+  for (y in inner_y_edges) {
+    patch_id <- paste0(as.character(x), "_", as.character(y))
+    patch <- sc_coord[which((sc_coord$imagerow > x) & (sc_coord$imagerow < x + patch_size) & 
+                              (sc_coord$imagecol > y) & (sc_coord$imagecol < y + patch_size)), ]
+    if (length(rownames(patch)) > 0) {
+      sc_coord[rownames(patch), ]$patch_id <- patch_id
+    }
+  }
+}
+sc_coord$cell_class <- as.vector(brain_celltrek$cell_type)
+patch_cell_types <- sc_coord[which(sc_coord$patch_id != ""), c("patch_id", "cell_class")]
+cellTypeTable <- table(patch_cell_types[])#patch_id*cell_class,每个patch中对应的cell_class数目
+cellType_stat <- as.data.frame(cellTypeTable)
+cellType_stat$cell_class <- factor(cellType_stat$cell_class)
+cellType_stat <- spread(data = cellType_stat,
+                        key = cell_class,
+                        value = Freq)
+A <- column_to_rownames(cellType_stat,var="patch_id")
+cellType_stat$cellnumber <- apply(A,1,sum)
+
+#write.table(cellTypeTable,file="/home/yll/scRNA-seq_Mapping_to_STdata/patch_size150_celltype.txt")
+counts <- t(as.matrix(brain_celltrek@assays$RNA@counts))
+patchTotalCells <- rowSums(cellTypeTable)#每个patch种总共的cell number
+cellTypeCount <- c()
+for (i in seq_len(length(rownames(cellTypeTable)))) {
+  patch_num_cell_types <- length(which(cellTypeTable[i, ] != 0))#每个patch中celltype的number
+  cellTypeCount <- append(cellTypeCount, patch_num_cell_types)
+}
+patches <- unique(sc_coord$patch_id[which(!sc_coord$patch_id == "")])
+patchGexp <- do.call(rbind, lapply(patches, function(patch) {
+  cells <- rownames(sc_coord[which(sc_coord$patch_id == patch), ])
+  mat <- as.matrix(counts[cells, ])
+  if (length(cells) == 1) {
+    patch_counts <- as.vector(mat)
+  }
+  else if (length(cells) > 1) {
+    patch_counts <- colSums(mat)
+  }
+  else if (length(cells) == 0) {
+    cat("WARNING:", bregma, "patch", patch, "had no cells in `counts` to use for simulated gene expression", 
+        "\n")
+  }
+  patch_counts
+}))
+rownames(patchGexp) <- patches
+#cell type proportion
+cellType_stat <- column_to_rownames(cellType_stat,var="patch_id")
+cellTypeProp <- as.matrix(cellType_stat[,-16])
+cellTypeProp <- t(apply(cellTypeProp,1,function(x){x/sum(x)}))
+#each spot coordinate
+patches <- unique(sc_coord$patch_id[which(!sc_coord$patch_id == "")])
+spot_coord.x <- NULL  #用来存储celltype的构成信息
+spot_coord.y <- NULL #用来存储每个位置对应的cellname信息
+
+for (k in patches){
+  
+  x <- strsplit(k,split="_")[[1]][1] %>% as.numeric() %>% round(3)
+  y <- strsplit(k,split="_")[[1]][2] %>% as.numeric() %>% round(3)
+  spot_coord.x <- rbind(spot_coord.x,x)
+  spot_coord.y <- rbind(spot_coord.y,y)
+  
+}
+
+new_coord <- data.frame(imagerow=spot_coord.x,imagecol=spot_coord.y)
+rownames(new_coord) <- patches
+st_counts <- patchGexp
+
+# #downsamping SequencingDepth 
+# set.seed(100)
+# #st_counts <- sim_brain_st@assays$spatial@counts
+# new.st_cnt <- downsampleMatrix(t(st_counts), prop=0.01)
+# nUMI_stnew <- colSums(new.st_cnt)
+# max(nUMI_stnew)
+# st_count_new <- as.matrix(new.st_cnt)
+
+#creat spatial seurat object
+cellTypeProp <- as.data.frame(cellTypeProp)
+sim_brain_st <- CreateSeuratObject(counts = st_count_new,
+                                   meta.data = cellTypeProp,
+                                   assay="spatial")
+sim_brain_st@images$coordinate <- new_coord
+
+#creat single cell seurat object
+sc_count_use <- counts[rownames(patch_cell_types), ]
+sc_meta_data_use <- brain_celltrek@meta.data[rownames(patch_cell_types), ]
+sc_coord_use <- sc_coord[rownames(patch_cell_types), ]
+sim_brain_celltrek <- CreateSeuratObject(counts = t(sc_count_use),
+                                         meta.data = sc_meta_data_use,
+                                         assay="RNA")
+sim_brain_celltrek@images$coordinate <- sc_coord_use
+
+saveRDS(brain_celltrek,file=paste0(output_path,'/brain_celltrek_data.Rds'))
+save(sim_brain_st,file=paste0(output_path,'/brain_spatialspot_data.Rds'))
+
+
