4d7c1ce69943d22ef2faedbf1f6bbd6c81a74559
luluyan0606 <87313381+luluyan0606@users.noreply.github.com>
2022-07-17 15:25:34 +0800

    decon_Tangram

---
+++ ST_Deconvolution/Tangram_pipeline.py
@@ -0,0 +1,12 @@
+### Tangram usage guide
+### system requirment: python >= 3.8.5
+### the package installation refer to https://github.com/broadinstitute/Tangram
+
+### Step-1: prepare conda environment for Tangram
+conda env create -f environment.yml
+
+### Step-2: install STRIDE package
+conda activate tangram-env
+pip install tangram-sc
+
+
